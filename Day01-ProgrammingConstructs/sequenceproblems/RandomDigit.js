656f6cbec3ae543d25c4a7be1cb4329176600f7a
Yaman Mahtha <yamanmahtha@gmail.com>
2025-03-04 11:52:24 +0530

    [YAMAN] add. Random Function Math.floor(Math.random() * 10); to get Single digit

---
+++ Day01-ProgrammingConstructs/sequenceproblems/RandomDigit.js
@@ -0,0 +1,6 @@
+function getRandomDigit() {
+    return Math.floor(Math.random() * 10);
+}
+// Generate and print the random digit
+const randomDigit = getRandomDigit(); 
+console.log("Random Single Digit:", randomDigit);
